00b0f2f0a6067f84bffa7b2ed75ac354e814b97a
Zeng <linzeng3456@gmail.com>
2023-06-13 17:16:47 -0700

    delete console.log

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://tuiter-node-server-app-wfc4.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
-console.log("-----api_base-----")
-console.log(API_BASE)
 const TUITS_API = `${API_BASE}/tuits`;
-console.log(TUITS_API)
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit)
