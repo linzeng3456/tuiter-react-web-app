d9c2547e383556cecb68ed2562efa9b055b5d57c
Zeng <linzeng3456@gmail.com>
2023-06-20 12:33:49 -0700

    test

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 // const TUITS_API = 'https://tuiter-node-server-app-wfc4.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
+console.log("--------TUITS_API-----");
+console.log(TUITS_API);
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit)
